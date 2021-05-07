e49460df1c2133b1e1421f6dd7b3fe4043650a41
AnjalBam <anjalbam81@gmail.com>
2021-05-07 17:39:48 +0545

    [feat] add  logger and everything for a website

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const app = require('express')();
+const cors = require('cors')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const PORT = 8080;
+
+app.use(cors())
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
     res.send('welcome to my website!!!!');
